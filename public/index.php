b019f40b6977eea4283983fdb2d8d2bf27f8a1ad
Matt Smith <dr_matt_smith@me.com>
2021-05-13 13:03:23 +0000

    added simple class and index

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/../src/Product.php';
+
+$p1 = new Product('hammer', 9.99);
+print $p1;
---
+++ src/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+class Product 
+{
+    public function __construct(
+        private $name,
+        private $price
+    )
+    {}
+
+    public function __toString()
+    {
+        return "(Product) name: $this->name, price: $this->price";
+    }
+}
